3abc59f5001a2973f8ddf4b49f75f7a6faab1cd5
Teacherhand123 <anyaoluo@gmail.com>
2025-04-01 21:15:28 +0800

    更新tocast样式

--- content.js
+++ content.js
@@ -29,6 +29,7 @@ chrome.runtime.onMessage.addListener((message) => {
             // toast.style.width = window.innerWidth
             toast.style.backgroundColor = "black"
             toast.style.width = "10%"
+            toast.style.borderRadius = "10px"
             toast.style.border = "solid 1px #0c59a6"
 
             for (const elem of streamArray) {
